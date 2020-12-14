9daa26eddb784169957c18941a49094f61458602
Faiq Ali <faiqueali017@gmail.com>
2020-12-14 21:56:07 +0500

    Typing animation added in WelcomeViewController

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -14,8 +14,20 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+        //For typing animation
+        titleLabel.text = ""
+        var charIndex = 0.0
+        
+        let titleText = "⚡️FlashChat"
+        for letter in titleText {
+            //print(0.1*charIndex, letter)
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) {(timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
+    
     }
     
 
